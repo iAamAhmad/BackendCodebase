33afc093e37a7663a4ed7324e22afbefb8e51553
iAamAhmad <aghazi1011@gmail.com>
2023-09-29 21:51:02 +0500

    backend-2

--- server-one/index.js
+++ server-one/index.js
@@ -1,13 +1,38 @@
 
-//  Here's script with both routes added.
-const express = require('express');
-const app = express();
-app.get('/', function (request, response) {
-   response.send('Home');
-});
-app.get('/about', function (request, response) {
-   response.send('Know More About Us');
-});
-app.listen(3000, function () {
-   console.log('Listening on port 3000');
-});
+// //  Here's script with both routes added.
+// /*
+// const express = require('express');
+// const app = express();
+// app.get('/', function (request, response) {
+//    response.send('Home');
+// });
+// app.get('/about', function (request, response) {
+//    response.send('Know More About Us');
+// });
+// app.listen(3000, function () {
+//    console.log('Listening on port 3000');
+// });
+// */
+
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+app.route('/book')
+  .get((req, res) => {
+    res.send('Get a random book')
+  })
+  .post((req, res) => {
+    res.send('Add a book')
+  })
+  .put((req, res) => {
+    res.send('Update the book')
+  })
