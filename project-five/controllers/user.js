742ee50f3001766a288c136e6be4111399f60fb7
iAamAhmad <aghazi1011@gmail.com>
2023-10-05 20:29:56 +0500

    crud

--- project-five/controllers/user.js
+++ project-five/controllers/user.js
@@ -1,5 +1,5 @@
-const Todo = require('../models/user')
-const createData = async () => {
+const Todo = require("../models/user");
+const createdata = async (req, res) => {
    try {
       const { name, email, password } = req.body;
       const todo = new Todo({
@@ -7,21 +7,68 @@ const createData = async () => {
          email,
          password,
       })
-      await todo.save()
-      resizeBy.json(todo)
-   }
-   catch (error) {
-      resizeBy.status(500).json({ error: error.message })
+      await todo.save();
+      res.json(todo);
+   } catch (err) {
+      res.status(500).json({ error: err.message })
    }
 }
-const getAllData = (req,) => {
+const getAllData = async (req, res) => {
    try {
-      const getTodos = await Todo.find({
-         res.json(getTodos)
-         //function!
-      })
+      const getTodos = await Todo.find({})
+      res.json(getTodos)
+   } catch (err) {
+      res.status(500).json({ error: err.message })
    }
-   catch () {
+}
 
+const getById = async (req, res) => {
+   try {
+      let dataAgainstId = await Todo.findById(req.params.id)
+      if (!dataAgainstId) {
+         return res.status(404).json({ message: 'Not Found' })
+      }
+      res.json(dataAgainstId)
+
+   } catch (err) {
+      res.status(500).json({ error: err.message })
    }
+}
+const deleteById = async (req, res) => {
+   try {
+      let dataAgainstId = await Todo.findByIdAndDeleteById(req.params.id)
+      if (!dataAgainstId) {
+         return res.status(404).json({ message: 'Not Found' })
+      }
+      response.json({ message: "Delete Success" })
+
+   } catch (err) {
+      res.status(500).json({ error: err.message })
+   }
+}
+
+const updateById = async (req, res) => {
+   try {
+      const { name, email, password } = request.body
+      let dataAgainstId = await Todo.findByIdAndUpdateById(req.params.id, {
+         name,
+         email,
+         password
+      },
+         { new: true }
+      )
+      if (!dataAgainstId) {
+         return res.status(404).json({ message: 'Not Found' })
+      }
+      res.json(dataAgainstId)
+
+   } catch (error) {
+      res.status(500).json({ error: error.message })
+   }
+}
+
+
+module.exports = {
+   createdata, getAllData, getById, deleteById,
+   updateById
 }
