62984c19f65d6c8ead5dec41f5ce01246048efa4
iAamAhmad <aghazi1011@gmail.com>
2023-10-08 20:46:46 +0500

    node-essentials

--- nodeJs-essentials/index.js
+++ nodeJs-essentials/index.js
@@ -7,11 +7,74 @@ let justSplit = hello.split('');
 console.log(justSplit);
 */
 
-const path=require('path');
+const path = require('path');
+const readline = require('readline');
+const fs = require('fs');
 
 
-const dir= path.join('/foo', 'bar', 'baz/asdf', 'quux', '..');
-console.log(dir);
 
-const norm1=path.parse('/home/user/dir/file.txt');
-console.log(norm1);
+
+// const dir = path.join('/foo', 'bar', 'baz/asdf', 'quux', '..');
+// console.log(dir);
+
+// const norm1 = path.parse('/home/user/dir/file.txt');
+// console.log(norm1);
+// console.log("Nodemon Started");
+/*
+const intervalTime = 3000;
+console.log('Starting a functions');
+const intervalFunction = () => {
+   console.log('Done');
+}
+setTimeout(intervalFunction, intervalTime);
+
+setInterval
+const settingInterval=1000;
+let currentTime=0;
+const incTime=()=>{
+   currentTime += settingInterval;
+   console.log(`Waiting for ${currentTime/2000} times`);
+}
+setInterval(incTime, settingInterval);
+*/
+
+const rl = readline.Interface({
+   input: process.stdin,
+   output: process.stdout
+});
+rl.question("What are you doing? \n", (answer) => {
+   console.log(`Your answer is : ${answer}`);
+});
+
+// Fs module is used to deal with files and systems.
+const files = fs.readdirSync("./");
+console.log(files);
+/*
+fs.readFile("./", "utf-8", (err, ipsum) => {
+   if (err) {
+      thro"w err;
+   } else {
+      console.log(ipsum);
+   }
+})
+*/
+// fs.readFile("./", "UTF-8", (err, file) => {
+//    if (err) {
+//       throw err;
+//    } else {
+//       console.log(file)
+//    }
+// })
+fs.readFile("./", "UTF-8", (err, file) => {
+   if (err) {
+      throw err;
+   } else {
+      console.log(file)
+   }
+})
+fs.appendFileSync();
+
+fs.mkdir();
+fs.mkdir();
+// We are making this change for wakatime.
+
