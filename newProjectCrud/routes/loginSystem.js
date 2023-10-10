98396417e3e14a2783fb935d33ccbaef57aaee10
iAamAhmad <aghazi1011@gmail.com>
2023-10-10 10:37:40 +0500

    restPassword

--- newProjectCrud/routes/loginSystem.js
+++ newProjectCrud/routes/loginSystem.js
@@ -1,10 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const loginSystemController = require("../controllers/loginSystem");
+const loginSystemController = require('../controllers/loginSystem');
+const passwordResetController = require('../controllers/passwordReset'); 
 
-//* creating route 
+router.post('/signUp', loginSystemController.signUpUser);
+router.post('/signIn', loginSystemController.loginUser);
+router.post('/requestPasswordReset', passwordResetController.requestPasswordReset);
+router.post('/resetPassword', passwordResetController.resetPassword);
 
-router.post("/signUp", loginSystemController.signUpUser);
-router.post("/login", loginSystemController.loginUser);
-//* Exporting 
+// Exporting
 module.exports = router;
